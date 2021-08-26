150dda38d6d0703099902cf67c5155617d1630ff
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-26 14:25:58 -0300

    Refatoração

--- src/br/com/zup/Exercicio1.java
+++ src/br/com/zup/Exercicio1.java
@@ -9,20 +9,20 @@ public class Exercicio1 {
         Scanner leitor = new Scanner(System.in);
 
         //recebendo e atribuindo os valores
-        System.out.println("Dígite o primeiro numero: ");
+        System.out.println("Dígite o primeiro número: ");
         double num1 = leitor.nextDouble();
-        System.out.println("Dígite o segundo numero: ");
+        System.out.println("Dígite o segundo número: ");
         double num2 = leitor.nextDouble();
 
 
         if (num1 > num2){
-            System.out.println("O número 1 é maior que o número 2.");
+            System.out.println("O primeiro número  é maior que o segundo número.");
         }
         else if (num1 == num2){
-            System.out.println("Os numeros são iguais.");
+            System.out.println("Os números são iguais.");
         }
         else{
-            System.out.println("O número 2 é maior que o número 1.");
+            System.out.println("O segundo número é maior que o primeiro número.");
         }
 
     }
