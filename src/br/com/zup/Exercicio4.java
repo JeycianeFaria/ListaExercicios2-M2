4df7aeadbc8e3a2ee1b73b23d79388e7b9a71901
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-08-27 11:47:52 -0300

    Refatoração no Exercicio 4

--- src/br/com/zup/Exercicio4.java
+++ src/br/com/zup/Exercicio4.java
@@ -13,26 +13,26 @@ public class Exercicio4 {
         double salarioAtual = leitor.nextDouble();
 
         //declaração das variaveis
-        String percentual20 = "20%";
-        String percentual15 = "15%";
-        String percentual10 = "10%";
-        String percentual5 = "5%";
-        double valorAumento20 = salarioAtual * 0.2;
-        double valorAumento15 = salarioAtual * 0.15;
-        double valorAumento10 = salarioAtual * 0.1;
-        double valorAumento5 = salarioAtual * 0.05;
+        double percentual20 = 0.2;
+        double percentual15 = 0.15;
+        double percentual10 = 0.1;
+        double percentual5 = 0.05;
+        double valorAumento20 = salarioAtual * percentual20;
+        double valorAumento15 = salarioAtual * percentual15;
+        double valorAumento10 = salarioAtual * percentual10;
+        double valorAumento5 = salarioAtual * percentual5;
 
 
         if (salarioAtual > 0 & salarioAtual <= 280){
             System.out.println("O seu salário atual é R$ " + salarioAtual);
-            System.out.println("O seu percentual de aumento será de " + percentual20);
+            System.out.println("O seu percentual de aumento será de 20% " );
             System.out.println("O valor do aumento será de R$ " + valorAumento20);
             double salarioAjustado = salarioAtual + valorAumento20;
             System.out.println("O seu sálario atualizado será de R$ " + salarioAjustado);
         }
         else if (salarioAtual > 280 & salarioAtual <= 700){
             System.out.println("O seu salário atual é R$ " + salarioAtual);
-            System.out.println("O seu percentual de aumento será de " + percentual15);
+            System.out.println("O seu percentual de aumento será de 15%" );
             System.out.println("O valor do aumento será de R$ " + valorAumento15);
             double salarioAjustado = salarioAtual + valorAumento15;
             System.out.println("O seu sálario atualizado será de R$ " + salarioAjustado);
@@ -40,7 +40,7 @@ public class Exercicio4 {
         }
         else if (salarioAtual > 700 & salarioAtual <= 1500){
             System.out.println("O seu salário atual é R$ " + salarioAtual);
-            System.out.println("O seu percentual de aumento será de " + percentual10);
+            System.out.println("O seu percentual de aumento será de 10% " );
             System.out.println("O valor do aumento será de R$ " + valorAumento10);
             double salarioAjustado = salarioAtual + valorAumento10;
             System.out.println("O seu sálario atualizado será de R$ " + salarioAjustado);
@@ -48,7 +48,7 @@ public class Exercicio4 {
         }
         else if (salarioAtual > 1500 ){
             System.out.println("O seu salário atual é R$ " + salarioAtual);
-            System.out.println("O seu percentual de aumento será de " + percentual5);
+            System.out.println("O seu percentual de aumento será de 5%" );
             System.out.println("O valor do aumento será de R$ " + valorAumento5);
             double salarioAjustado = salarioAtual + valorAumento5;
             System.out.println("O seu sálario atualizado será de R$ " + salarioAjustado);
